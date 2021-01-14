895b40ac8ca2d3ae85b9e39ef06e0f092f6fea0b
majda107 <majda.trpkos@gmail.com>
2021-01-14 17:28:22 +0100

    edit file page style

--- MongoFS/Services/StateService.cs
+++ MongoFS/Services/StateService.cs
@@ -5,6 +5,26 @@ namespace MongoFS.Services
 {
     public class StateService
     {
+        public static readonly string[] FILE_TYPES = new[]
+        {
+            "csv",
+            "html",
+            "txt",
+            "vimscript",
+            "css",
+            "js",
+            "rb",
+            "go"
+        };
+
+        public static string ToMonacoType(string type) => type switch
+        {
+            "css" => "text/css",
+            "html" => "text/html",
+            _ => "text"
+        };
+
+
         public event EventHandler<int> Deleted;
 
         public void FireDeleted(int size) => this.Deleted?.Invoke(this, size);
